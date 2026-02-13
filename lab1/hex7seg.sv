a1d8c60f74d1229374a65f080b2d28dc81ad129c
kno2117 <kno2117@micro25.ee.columbia.edu>
2026-02-13 11:06:46 -0500

    Modiefied.

--- lab1/hex7seg.sv
+++ lab1/hex7seg.sv
@@ -1,26 +1,28 @@
-module hex7seg(input logic  [3:0] a,
-	       output logic [6:0] y);
+module hex7seg(
+    input  logic [3:0] a,
+    output logic [6:0] y
+);
 
-   /* Replace this comment and the code below it with your solution */
-   // assign y = { a[2:0], a[3:0] };
-   always_comb
-	   case(a)
-		   4'd0: 	y = 7'b100_0000;
-		   4'd1:        y = 7'b111_1001;
-		   4'd2:        y = 7'b010_0100;
-		   4'd3:        y = 7'b011_0000;
-		   4'd4:        y = 7'b001_1001;
-		   4'd5:        y = 7'b001_0010;
-		   4'd6:        y = 7'b000_0010;
-		   4'd7:        y = 7'b111_1000;
-		   4'd8:        y = 7'b000_0000;
-		   4'd9:        y = 7'b001_0000;
-		   4'd10:       y = 7'b000_1000;
-		   4'd11:       y = 7'b000_0011;
-		   4'd12:       y = 7'b100_0110;
-		   4'd13:       y = 7'b010_0001;
-		   4'd14:       y = 7'b000_0110;
-		   4'd15:       y = 7'b000_1110;
-		   default:	y = 7'b111_1111;
-	   endcase
+    always_comb begin
+        case (a)
+            4'h0: y = 7'h40;
+            4'h1: y = 7'h79;
+            4'h2: y = 7'h24;
+            4'h3: y = 7'h30;
+            4'h4: y = 7'h19;
+            4'h5: y = 7'h12;
+            4'h6: y = 7'h02;
+            4'h7: y = 7'h78;
+            4'h8: y = 7'h00;
+            4'h9: y = 7'h10;
+            4'ha: y = 7'h08;
+            4'hb: y = 7'h03;
+            4'hc: y = 7'h46;
+            4'hd: y = 7'h21;
+            4'he: y = 7'h06;
+            4'hf: y = 7'h0e;
+            default: y = 7'h7f;
+        endcase
+    end
+
 endmodule
